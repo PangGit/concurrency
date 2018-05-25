6b3ae9b60ee2a32b3759e309c91e9cba74ac2ab4
Dab <pang2081@163.com>
2018-05-25 18:46:53 +0800

    0

--- src/main/java/com/algorithm/hashmap/HashMapTest.java
+++ src/main/java/com/algorithm/hashmap/HashMapTest.java
@@ -1,5 +1,7 @@
 package com.algorithm.hashmap;
 
+import java.util.HashMap;
+
 /**
  * @author dab
  * @version 1.0.0
@@ -7,4 +9,40 @@ package com.algorithm.hashmap;
  * @Date 2018/5/24 18:04
  */
 public class HashMapTest {
+
+    HashMap<String, Object> m = new HashMap<String, Object>();
+
+    public void setM() {
+        m.put("a", "1");
+        m.put("b", "2");
+        m.put("c", "3");
+        m.put("d", "4");
+        m.put("e", "5");
+        m.put("f", "6");
+        m.put("g", "7");
+        m.put("h", "8");
+        m.put("i", "9");
+        m.put("j", "10");
+        m.put("k", "11");
+        m.put("o", "12");
+        m.put("p", "13");
+        m.put("q", "14");
+        m.put("r", "15");
+        m.put("s", "16");
+        m.put("t", "17");
+        m.put("u", "18");
+        m.put("v", "19");
+        m.put("w", "20");
+        m.put("x", "21");
+        m.put("y", "22");
+        m.put("z", "23");
+    }
+
+    public static void main(String[] args) {
+
+
+
+    }
+
+
 }
