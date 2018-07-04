9c20104c07a96c66495de658d0ae37624c0e3601
Dab <PangGit@users.noreply.github.com>
2018-07-04 21:43:37 +0800

    1

--- src/main/java/book/chapter02/Counter.java
+++ src/main/java/book/chapter02/Counter.java
@@ -6,9 +6,6 @@ import java.util.concurrent.atomic.AtomicInteger;
 
 /**
  * 计数器
- *
- * @author tengfei.fangtf
- * @version $Id: Snippet.java, v 0.1 2015-7-31 下午11:32:42 tengfei.fangtf Exp $
  */
 public class Counter {
 
@@ -24,7 +21,7 @@ public class Counter {
 
         for (int j = 0; j < 100; j++) {
             Thread t = new Thread(() -> {
-                for (int i = 0; i < 10000; i++) {
+                for (int i = 0; i < 100; i++) {
                     cas.count();
                     cas.safeCount();
                 }
@@ -52,7 +49,7 @@ public class Counter {
      * 使用CAS实现线程安全计数器
      */
     private void safeCount() {
-        for (; ; ) {
+        while (true) {
             int i = atomicI.get();
             boolean suc = atomicI.compareAndSet(i, ++i);
             if (suc) {
