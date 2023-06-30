b3c5ea9c9f7d8863aecca405429da244938bcf67
South Mountain <PangGit@users.noreply.github.com>
2023-06-30 23:33:05 +0800

    线程池

--- src/main/java/com/thread/pool/ThreadPoolUtil.java
+++ src/main/java/com/thread/pool/ThreadPoolUtil.java
@@ -1,6 +1,9 @@
 package com.thread.pool;
 
+import com.google.common.util.concurrent.ThreadFactoryBuilder;
+
 import java.util.concurrent.LinkedBlockingDeque;
+import java.util.concurrent.ThreadFactory;
 import java.util.concurrent.ThreadPoolExecutor;
 import java.util.concurrent.TimeUnit;
 
@@ -14,12 +17,16 @@ public class ThreadPoolUtil {
 
     private static final int CPU_COUNT = Runtime.getRuntime().availableProcessors();
 
+    private static final ThreadFactory namedThreadFactory
+            = new ThreadFactoryBuilder().setNameFormat("demo-pool-%d").build();
+
     public static ThreadPoolExecutor pool = new ThreadPoolExecutor(
             CPU_COUNT,
             CPU_COUNT * 2,
             3,
             TimeUnit.SECONDS,
             new LinkedBlockingDeque<>(1024),
+            namedThreadFactory,
             new ThreadPoolExecutor.CallerRunsPolicy());
 
 }
